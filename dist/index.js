06c495548c9b059adc9b17850e07a226565e20e3
haraldox <hnaumann+github@gmail.com>
2018-03-04 16:51:36 +0100

    update dist

--- dist/index.js
+++ dist/index.js
@@ -26865,7 +26865,6 @@ function sortableHandle(WrappedComponent) {
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
-exports.DragLayer = exports.arrayMove = exports.arrayInsert = exports.sortableHandle = exports.sortableElement = exports.sortableContainer = exports.SortableHandle = exports.SortableElement = exports.SortableContainer = undefined;
 
 var _SortableContainer = require('./SortableContainer');
 
@@ -26887,15 +26886,17 @@ var _DragLayer2 = _interopRequireDefault(_DragLayer);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-exports.SortableContainer = _SortableContainer2.default;
-exports.SortableElement = _SortableElement2.default;
-exports.SortableHandle = _SortableHandle2.default;
-exports.sortableContainer = _SortableContainer2.default;
-exports.sortableElement = _SortableElement2.default;
-exports.sortableHandle = _SortableHandle2.default;
-exports.arrayInsert = _utils.arrayInsert;
-exports.arrayMove = _utils.arrayMove;
-exports.DragLayer = _DragLayer2.default;
+exports.default = {
+    SortableContainer: _SortableContainer2.default,
+    SortableElement: _SortableElement2.default,
+    SortableHandle: _SortableHandle2.default,
+    sortableContainer: _SortableContainer2.default,
+    sortableElement: _SortableElement2.default,
+    sortableHandle: _SortableHandle2.default,
+    arrayInsert: _utils.arrayInsert,
+    arrayMove: _utils.arrayMove,
+    DragLayer: _DragLayer2.default
+};
 
 },{"./DragLayer":330,"./SortableContainer":333,"./SortableElement":334,"./SortableHandle":335,"./utils":337}],337:[function(require,module,exports){
 'use strict';
